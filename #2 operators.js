0b41159b2b50f98246776864a874e414cc434c4e
Saikumar-R <70192468+Saikumar-R@users.noreply.github.com>
2024-10-22 21:41:11 +0530

    Add files via upload

---
+++ #2 operators.js
@@ -0,0 +1,50 @@
+//Assignment Operator (=)
+
+var data = "hello";
+
+//Arithmatic Operators (+, -, *, /, %, **)
+
+// console.log(5 + 5);
+// console.log(5 - 5);
+// console.log(5 * 5);
+// console.log(5 / 5);
+// console.log(5 % 5);
+// console.log(5 ** 5);
+
+//Arthimatic Assignment (+=, -=, *=, /=, %=, **=)
+
+// let x = 5;
+// let y = 2;
+
+// x = x+y; // x+=y
+
+// console.log(x**=y);
+// console.log(x);
+
+//comparison operators(==, ===, !=, !==, >,<, >=, <=)
+//  let x = 5; //number
+//  let y = "5"; //string
+// let z = 8;
+// console.log(x === y); //false 
+// console.log(x != y); //false
+// console.log(x !== y); //true 
+// console.log(x > z ) //false
+
+//Logical operators (&&, ||, !)
+
+// let andOp = ((5 > 8) && (5 < 8)) //flase true
+// console.log(andOp);
+
+// let orOp = ((5 > 8) || (5 < 8))  //flase true
+// console.log(orOp)
+
+// const typeData = false; //boolen 
+// console.log(!typeData);
+
+//ternary Operator ( ? )
+// if(true){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+console.log("op" === "opp" ? true : false);
---
+++ #3 dataTypes.js
@@ -0,0 +1,57 @@
+//Data Types In JavaScript
+//Static Typing vs Dynamic Typing
+//Primitive vs Reference
+    //Stack and Heap
+//Bonus Tip 
+
+// /************* Primitive Data Types *********************/ --> Stack
+    //1.String
+    //2.Number
+    //3.Boolean
+    //4.Null
+    //5.Undefined
+    //6.Symbol
+    //7.BigInt
+// /************* Reference Data Types *********************/ --> Heap
+    //1.Arrays
+    //2.Objects
+
+let input;
+
+// input = 'hello JS'; // string
+
+// input = 123; //number 
+
+// input = "123"; // string
+
+// input = true //boolean
+// input = false //boolean
+
+// input = "false" //string
+
+// input = null;
+
+// input = undefined;
+
+// let sym1 = Symbol(true);
+// let sym2 = Symbol(true);
+
+// input = 21161314641211n;
+
+// input = [2,4,8,6];
+// input = {
+//     "name":"JSDC",
+//     "Subs":84
+// }
+
+const x = 5; //number
+const y = "6"; //string
+
+// let output = input;
+
+// // console.log(output, "is a", typeof(output));
+
+// console.log(output.name);
+
+console.log(x-y)
+
