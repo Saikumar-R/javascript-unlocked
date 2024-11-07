87bca0626906787a7a0b66d4efa7452025eea5e5
Saikumar-R <70192468+Saikumar-R@users.noreply.github.com>
2024-11-07 12:53:45 +0530

    Add files via upload

---
+++ #4 type-conversion_coercion.js
@@ -0,0 +1,38 @@
+//Type Conversion
+//Type Coercion
+//difference b/w both.
+
+let input;
+
+// input = 2 + Number("2");
+
+//String to number
+
+    // input = parseInt(input);
+    // input = +input
+    // input = Number(input);
+
+//number to string
+    // input = input.toString();
+    // input = String(input);
+
+// string to decimal 
+
+    // input = parseFloat(input);
+
+//number to boolean
+    // 0 --> false
+    //1 --> true
+    //input = Boolean(input);
+
+
+// input = 1 * null; //1+0=1
+
+// input = 1 + true // 1 + 1  = 2
+// input = 1 + false //1+0=1
+input = 1 + NaN; //not a number 
+
+input = undefined + undefined;
+
+input = "hhjb" ** 2
+console.log(input, typeof input);
